c41d81d93067c964a770bda9edbd327da37b1b4d
Ahren-with-an-h <agroesch@gmail.com>
2020-07-03 21:53:29 -0400

    QOL changes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # background-generator
+
+Test background gradients or try random ones. Control how bright the random ones are.
--- script.js
+++ script.js
@@ -12,30 +12,34 @@ function setGradient() {
 		'to right' + ', ' + 
 		color1.value + ', ' + 
 		color2.value + ')';
-
+		// Put the CSS code on screen for current background
 		css.textContent = body.style.background + ';' ;
 }
 
-function randomColor(brightness){ 
-// creates random hex colors with a minimum brightness
-// source: https://stackoverflow.com/questions/1152024/best-way-to-generate-a-random-color-in-javascript/1152508
+/**
+ * creates random hex colors with a minimum brightness
+ * source: https://stackoverflow.com/questions/1152024/best-way-to-generate-a-random-color-in-javascript/1152508
+ */
+function createRandomColor(brightness){ 
+	brightness = parseInt(brightness); // make sure brightness is an integer
 	function randomChannel(brightness){
 		var r = 255-brightness;
 		var n = 0|((Math.random() * r) + brightness);
 		var s = n.toString(16);
-		return (s.length==1) ? '0'+s : s; // if one digit padd with 0
+		return (s.length==1) ? '0'+s : s ; // if one digit padd with 0
 	}
 	return '#' + randomChannel(brightness) + randomChannel(brightness) + randomChannel(brightness);
 } 
 
 function randomizeColors() {
-	color1.value = randomColor(parseInt(minBrightness.value));
-	color2.value = randomColor(parseInt(minBrightness.value));
+	color1.value = createRandomColor(minBrightness.value);
+	color2.value = createRandomColor(minBrightness.value);
 	setGradient();
 }
 
-setGradient(); // Initially set Gradient
-
+// Initially set Gradient
+setGradient(); 
+// Add event listeners to set gradient when user changes color or clicks RANDOM
 color1.addEventListener('input', setGradient);
 color2.addEventListener('input', setGradient);
 randBtn.addEventListener('click', randomizeColors);
